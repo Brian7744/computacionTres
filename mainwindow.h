7a5f542ec8a78a24a413a45dae75b221af843aa8
BRIAN HAFFNER <brianhaffner1998@gmail.com>
2021-08-13 11:06:59 -0300

    Segunda clase 3 componentes

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <qpaintbox.h>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,16 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onQTimer1();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QTimer *QTimer1;
+    int counter;
 };
 #endif // MAINWINDOW_H
